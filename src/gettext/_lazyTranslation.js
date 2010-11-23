b8c128e5f05fc54d70e409c9b77fc78d77d8c38c
David Aurelio <david@uxebu.com>
2010-11-23 09:20:42 +0100

    Adding mixin with support for lazy translations

---
+++ src/gettext/_lazyTranslation.js
@@ -0,0 +1,57 @@
+(function(/**gettext.TextDomain.prototype*/tdp){
+    tdp.LazyTranslation = function(domain, args) {
+        this._domain = domain;
+        this._args = args;
+    };
+
+    tdp.LazyTranslation.prototype.toString = function() {
+        var domain = this._domain;
+        return domain.ngettext.apply(domain, this._args);
+    };
+
+    tdp.LazyTranslation.prototype.valueOf = tdp.LazyTranslation.prototype.toString;
+
+    tdp._toBind.push("gettextLazy", "ngettextLazy");
+
+    /**
+     * @param {String} [context] The context of the message
+     * @param {String} message The message to translate
+     * @param {Object} [options] Options
+     * @returns {String}
+     */
+    tdp.gettextLazy = function(context, message, options) {
+        // arguments normalization
+        if (typeof options === "undefined") { // less than 3 args received
+            if (typeof message !== "string") { // no context received
+                message = context;
+                options = message;
+                context = null;
+            }
+        }
+
+        return new this.LazyTranslation(this, [context, message, null, 1, options]);
+    };
+
+    /**
+     * @param {String} [context] The context of the message
+     * @param {String} singularMsg The singular message to translate
+     * @param {String} pluralMsg The plural message to translate
+     * @param {Number} n
+     * @param {Object} [options] Options
+     * @returns {String}
+     */
+    tdp.ngettextLazy = function() {
+        // arguments normalization
+        if (typeof options === "undefined") { // less than 5 args received
+            if (typeof n !== "number") { // if not a number, no context given
+                singularMsg = context;
+                pluralMsg = singularMsg;
+                n = pluralMsg;
+                options = n;
+                context = null;
+            }
+        }
+
+        return new this.LazyTranslation(this, arguments);
+    };
+}(gettext.TextDomain.prototype));
